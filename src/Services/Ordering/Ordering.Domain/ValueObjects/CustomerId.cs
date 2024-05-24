b9b6d48040121db31874f46f8600c8901b4aa84b
DongPN <ngocdong.pham@wiztechww.com>
2024-05-24 21:57:35 +0700

    end section 14

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.ValueObjects;
+public record CustomerId
+{
+    public CustomerId(Guid value)
+    {
+        Value = value;
+    }
+    public Guid Value { get; }
+
+    public static CustomerId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("CustomerId cannot be empty.");
+        }
+        return new CustomerId(value);
+    }
+}
