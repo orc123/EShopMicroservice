71e650e0c061247b277760408369f895f8c72255
DongPN <ngocdong.pham@wiztechww.com>
2024-05-31 21:25:37 +0700

    End Session 16

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.UpdateOrder;
+public record UpdateOrderCommand(OrderDto Order) : ICommand<UpdateOrderResult>;
+
+public record UpdateOrderResult(bool IsSuccess);
+
+public class UpdateOrderCommandVAlidator : AbstractValidator<UpdateOrderCommand>
+{
+    public UpdateOrderCommandVAlidator()
+    {
+        RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("CustomerId is required");
+        RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("CustomerId should not be empty");
+    }
+}
